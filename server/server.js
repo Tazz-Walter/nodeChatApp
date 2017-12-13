82d9c6b3f68a7e6e34dd1e97fd3e6e51497ecfb8
Wally <wolaondo@gmail.com>
2017-12-13 15:46:51 -0300

    Saying hello to new user, and alert the other ones

--- server/server.js
+++ server/server.js
@@ -15,16 +15,39 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to chat App',
+    createdAt: new Date().getTime()
+  });
+  socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'New user Joined!',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
-    io.emit('newMessage',{
+    io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //    createdAt: new Date().getTime()
+    // });
   });
+
   socket.on('disconnect', () => {
-    console.log('Disconnected from client');
+    console.log('User Disconnected ');
+    socket.broadcast.emit('newMessage',{
+      from: 'Admin',
+      text: 'One user has left the Room',
+      createdAt: new Date().getTime()
+    });
   });
 });
 
