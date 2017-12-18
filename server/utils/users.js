7f0e09134a69122c2aa2a5b9a191076fd720bbcd
Wally <wolaondo@gmail.com>
2017-12-18 14:32:29 -0300

    adding methods to Users and testing that methods

--- server/utils/users.js
+++ server/utils/users.js
@@ -1,24 +1,34 @@
-[{
-  id: '/#234asasdf',
-  name: 'Walter',
-  room: 'Developers'
-}]
+// [{
+//   id: '/#234asasdf',
+//   name: 'Walter',
+//   room: 'Developers'
+// }]
 
-// methods that we need
-// addUser(id, name, room)
-// removeUser(id)
-// getUser(id)
-// getUserList(room)
 class Users {
   constructor () {
     this.users = [];
   }
+  //methods
   addUser (id, name, room) {
     var user = {id, name, room};
     this.users.push(user);
     return user;
   }
-
+  removeUser (id) {
+    var user = this.getUser(id);
+    if (user) {
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+    return user;
+  }
+  getUser (id) {
+    return this.users.filter((user) => user.id === id)[0];
+  }
+  getUserList(room) {
+    var usuarios= this.users.filter((user) => user.room === room);
+    var nombresArray = usuarios.map((user) => user.name);
+    return nombresArray;
+  }
 }
 
 module.exports = {Users};
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -1,6 +1,22 @@
 const {Users} = require('./users');
 
 describe('Users', () => {
+  beforeEach(()=>{
+    usuarios = new Users();
+    usuarios.users = [{
+      id: '1',
+      name: 'walter',
+      room: 'deveps'
+    },{
+      id: '2',
+      name: 'debo',
+      room: 'deveps2'
+    },{
+      id: '3',
+      name: 'alice',
+      room: 'deveps'
+    }];
+  })
 
   it('should add new user', () => {
     var usuarios = new Users();
@@ -12,6 +28,38 @@ describe('Users', () => {
     };
     var resUser = usuarios.addUser(newUser.id, newUser.name, newUser.room);
     expect(usuarios.users).toEqual([newUser]);
+  });
+
+  it('should remove a user from array', () => {
+    var elUsuario = usuarios.removeUser('1');
+    var listaEsperada = [{id: '2', name: 'debo', room: 'deveps2'},{id: '3', name: 'alice', room: 'deveps'}];
+    expect(usuarios.users).toEqual(listaEsperada);
+    expect(elUsuario).toEqual({id: '1', name: 'walter', room: 'deveps'});
+  });
+
+  it('should not remove a user from array', () => {
+    var elUsuario = usuarios.removeUser('4');
+    expect(elUsuario).toBeFalsy();
+  });
+
+  it('should find user by Id', () => {
+    var nuevaLista = usuarios.getUser('2');
+    expect(nuevaLista).toEqual(usuarios.users[1]);
+  });
+
+  it('should not find user with incorrect Id', () => {
+    var nuevaLista = usuarios.getUser('5');
+    expect(nuevaLista).toBeFalsy();
+  });
+
+  it('should return names of deveps2 rooms', () => {
+    var listaUsuarios = usuarios.getUserList('deveps2');
+    expect(listaUsuarios).toEqual(['debo']);
+  });
+
+  it('should return names of deveps rooms', () => {
+    var listaUsuarios = usuarios.getUserList('deveps');
+    expect(listaUsuarios).toEqual(['walter', 'alice']);
   });
 
 });
